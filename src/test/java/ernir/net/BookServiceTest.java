85eed9ce10cfd3b098a5dc7384ebf1bcc4eda88b
Ernir <e.ernir@gmail.com>
2024-09-29 13:44:14 +0200

    Test the rest of the service methods

--- src/test/java/ernir/net/BookServiceTest.java
+++ src/test/java/ernir/net/BookServiceTest.java
@@ -6,18 +6,19 @@ import static org.mockito.Mockito.when;
 
 import ernir.net.books.data.BookCsvLine;
 import ernir.net.books.data.BookCsvParser;
+import ernir.net.books.models.Author;
+import ernir.net.books.models.Book;
+import ernir.net.books.models.SearchResult;
+import ernir.net.books.models.Series;
+import ernir.net.books.models.SeriesMembership;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
-import ernir.net.books.models.Series;
-import ernir.net.books.models.SeriesMembership;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class BookServiceTest {
 
-  private static final BookCsvLine DEATHSEED_CSV =
+  private static final BookCsvLine DEATHSEED =
       new BookCsvLine(
           "214240402",
           "Deathseed (The Weirkey Chronicles Book 8)",
@@ -35,7 +36,7 @@ class BookServiceTest {
           Optional.empty(),
           Optional.of(LocalDate.parse("2024-07-28")),
           Optional.of(LocalDate.parse("2024-07-21")));
-  private static final BookCsvLine WAKESPIRE_CSV =
+  private static final BookCsvLine WAKESPIRE =
       new BookCsvLine(
           "195698323",
           "Wakespire (The Weirkey Chronicles #7)",
@@ -53,7 +54,7 @@ class BookServiceTest {
           Optional.of(2023),
           Optional.of(LocalDate.parse("2024-05-02")),
           Optional.of(LocalDate.parse("2024-04-24")));
-  private static final BookCsvLine THE_ENGINEER_CSV =
+  private static final BookCsvLine THE_ENGINEER =
       new BookCsvLine(
           "201127814",
           "The Engineer (The Last Horizon, #2)",
@@ -71,7 +72,7 @@ class BookServiceTest {
           Optional.of(2023),
           Optional.of(LocalDate.parse("2024-04-23")),
           Optional.of(LocalDate.parse("2024-04-18")));
-  private static final BookCsvLine UNFETTERED_CSV =
+  private static final BookCsvLine UNFETTERED =
       new BookCsvLine(
           "15832300",
           "Unfettered (Unfettered, #1)",
@@ -112,7 +113,7 @@ class BookServiceTest {
           Optional.of(2013),
           Optional.of(LocalDate.parse("2014-02-25")),
           Optional.of(LocalDate.parse("2014-02-25")));
-  private static final BookCsvLine SEVEN_OF_INFINITIES_CSV =
+  private static final BookCsvLine SEVEN_OF_INFINITIES =
       new BookCsvLine(
           "56254221",
           "Seven of Infinities (Xuya Universe)",
@@ -149,60 +150,67 @@ class BookServiceTest {
           Optional.of(LocalDate.parse("2014-04-29")),
           Optional.of(LocalDate.parse("2014-04-29")));
 
+  private static String apply(SearchResult b) {
+    Book book = (Book) b;
+    return book.titleFull();
+  }
+
   @Test
   void findAllBooks() {
     BookCsvParser parser = mock();
     List<BookCsvLine> csvs =
         List.of(
-            DEATHSEED_CSV,
-            WAKESPIRE_CSV,
-            THE_ENGINEER_CSV,
-            UNFETTERED_CSV,
-            SEVEN_OF_INFINITIES_CSV,
+            DEATHSEED,
+            WAKESPIRE,
+            THE_ENGINEER,
+            UNFETTERED,
+            SEVEN_OF_INFINITIES,
             BRAVE_NEW_WORLD_CSV);
     when(parser.getBooks()).thenReturn(csvs);
 
     BookService bookService = new BookService(parser);
+    List<Book> books = bookService.findAllBooks();
 
-    var books = bookService.findAllBooks();
-    // TODO make real assertions
-    assertThat(books).hasSize(6);
+    assertThat(books)
+        .extracting(Book::titleShort)
+        .containsExactly(
+            "Deathseed",
+            "Wakespire",
+            "The Engineer",
+            "Unfettered",
+            "Seven of Infinities",
+            "Brave New World");
   }
 
   @Test
-  void findAllBooks_none_available() {
+  void findAllBooksNoneAvailable() {
     BookCsvParser parser = mock();
-
     when(parser.getBooks()).thenReturn(List.of());
 
     BookService bookService = new BookService(parser);
+    List<Book> books = bookService.findAllBooks();
 
-    var books = bookService.findAllBooks();
     assertThat(books).isEmpty();
   }
 
   @Test
   void getSeriesByName() {
     BookCsvParser parser = mock();
-
-    when(parser.getBooks()).thenReturn(List.of(DEATHSEED_CSV, WAKESPIRE_CSV));
+    when(parser.getBooks()).thenReturn(List.of(DEATHSEED, WAKESPIRE));
 
     BookService bookService = new BookService(parser);
-
-    var series = bookService.getSeriesByName("The Weirkey Chronicles");
+    Optional<Series> series = bookService.getSeriesByName("The Weirkey Chronicles");
 
     assertThat(series).hasValue(new Series("The Weirkey Chronicles"));
   }
 
   @Test
-  void getSeriesByName_not_found() {
+  void getSeriesByNameNotFound() {
     BookCsvParser parser = mock();
-
-    when(parser.getBooks()).thenReturn(List.of(DEATHSEED_CSV, WAKESPIRE_CSV));
+    when(parser.getBooks()).thenReturn(List.of(DEATHSEED, WAKESPIRE));
 
     BookService bookService = new BookService(parser);
-
-    var series = bookService.getSeriesByName("The Weird Chronicles");
+    Optional<Series> series = bookService.getSeriesByName("The Weird Chronicles");
 
     assertThat(series).isEmpty();
   }
@@ -210,11 +218,9 @@ class BookServiceTest {
   @Test
   void findBooksOfSeries() {
     BookCsvParser parser = mock();
-
-    when(parser.getBooks()).thenReturn(List.of(DEATHSEED_CSV, WAKESPIRE_CSV));
+    when(parser.getBooks()).thenReturn(List.of(DEATHSEED, WAKESPIRE));
 
     BookService bookService = new BookService(parser);
-
     Series theWeirkeyChronicles = new Series("The Weirkey Chronicles");
     List<SeriesMembership> series = bookService.findBooksOfSeries(theWeirkeyChronicles);
 
@@ -225,18 +231,73 @@ class BookServiceTest {
   }
 
   @Test
-  @Disabled
-  void findBookOfSeriesMembership() {}
+  void findBookOfSeriesMembership() {
+    BookCsvParser parser = mock();
+    when(parser.getBooks()).thenReturn(List.of(DEATHSEED, WAKESPIRE));
+
+    BookService bookService = new BookService(parser);
+
+    SeriesMembership theWeirkeyChronicles8 =
+        new SeriesMembership(new Series("The Weirkey Chronicles"), Optional.of("8"));
+    assertThat(bookService.findBookOfSeriesMembership(theWeirkeyChronicles8))
+        .extracting(Book::titleShort)
+        .isEqualTo("Deathseed");
+  }
 
   @Test
-  @Disabled
-  void testFindBookByTitle() {}
+  void testFindBookByTitle() {
+    BookCsvParser parser = mock();
+    when(parser.getBooks()).thenReturn(List.of(DEATHSEED, WAKESPIRE));
+
+    BookService bookService = new BookService(parser);
+
+    assertThat(bookService.findBookByTitle("Deathseed (The Weirkey Chronicles Book 8)"))
+        .hasValueSatisfying(book -> assertThat(book).extracting(Book::id).isEqualTo(214240402L));
+  }
 
   @Test
-  @Disabled
-  void testSearch() {}
+  void testSearch() {
+    BookCsvParser parser = mock();
+    List<BookCsvLine> csvs =
+        List.of(
+            DEATHSEED,
+            WAKESPIRE,
+            THE_ENGINEER,
+            UNFETTERED,
+            SEVEN_OF_INFINITIES,
+            BRAVE_NEW_WORLD_CSV);
+    when(parser.getBooks()).thenReturn(csvs);
+
+    BookService bookService = new BookService(parser);
+    List<SearchResult> xSearchResults = bookService.search("x");
+
+    assertThat(xSearchResults).hasSize(2);
+    assertThat(xSearchResults)
+        .first()
+        .isInstanceOf(Book.class)
+        .extracting(book -> ((Book) book).titleFull())
+        .isEqualTo("Seven of Infinities (Xuya Universe)");
+    assertThat(xSearchResults)
+        .last()
+        .isInstanceOf(Author.class)
+        .extracting(author -> ((Author) author).fullName())
+        .isEqualTo("Aldous Huxley");
+  }
 
   @Test
-  @Disabled
-  void testGetBooksByAuthor() {}
+  void testGetBooksByAuthor() {
+    BookCsvParser parser = mock();
+    List<BookCsvLine> csvs = List.of(DEATHSEED, WAKESPIRE, THE_ENGINEER);
+    when(parser.getBooks()).thenReturn(csvs);
+    Author author = new Author("Sarah Lin", "Sarah", "Lin");
+
+    BookService bookService = new BookService(parser);
+    List<Book> books = bookService.getBooksByAuthor(author);
+
+    assertThat(books)
+        .hasSize(2)
+        .extracting(Book::titleShort)
+        .containsExactly("Deathseed", "Wakespire")
+        .doesNotContain("The Engineer");
+  }
 }
