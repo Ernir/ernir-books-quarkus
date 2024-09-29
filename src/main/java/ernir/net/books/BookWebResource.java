8e5580b1dfc65b0eaa08306c4f6b1d8727e74d13
Ernir <e.ernir@gmail.com>
2024-09-29 15:03:29 +0200

    Make a template render all the books

---
+++ src/main/java/ernir/net/books/BookWebResource.java
@@ -0,0 +1,32 @@
+package ernir.net.books;
+
+import ernir.net.books.models.Book;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("books")
+public class BookWebResource {
+  private final BookService bookService;
+
+  BookWebResource(BookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @CheckedTemplate
+  public static class Templates {
+    public static native TemplateInstance books(List<Book> books);
+  }
+
+  @GET
+  @Path("")
+  @Produces(MediaType.TEXT_HTML)
+  public TemplateInstance get() {
+    return Templates.books(bookService.findAllBooks());
+  }
+}
